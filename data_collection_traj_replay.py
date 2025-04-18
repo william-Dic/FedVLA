89c2cd385205b235c39c3daee6700d94975f16c9
Guanming Wang <84255909+william-Dic@users.noreply.github.com>
2025-04-18 19:55:46 +0800

    Update data_collection_traj_replay.py

--- data_collection_traj_replay.py
+++ data_collection_traj_replay.py
@@ -195,6 +195,7 @@ class TeachingTest(Helper):
             with open(state_file, 'w') as f:
                 json.dump(state_data, f, indent=2)
             
+            print(f"Saved trajectory to {episode_folder}"))
             self.echo(f"Saved trajectory to {episode_folder}")
         
         self.echo("Finish play")
